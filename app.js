32f7a9211568115acdcfcb807b3f87a55f31347f
EveK0 <kaylanefs39@gmail.com>
2024-03-20 23:07:44 -0300

    🚧✅feat: base of project

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const routes = require('./routes/index');
+const users = require('./routes/users');
+require('dotenv').config();
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+
+module.exports = app;
+
